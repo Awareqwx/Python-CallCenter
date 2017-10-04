82d0e834b82b28b5add4ef725653edfe34e1480d
Awareqwx <benpeters97@yahoo.com>
2017-10-04 15:34:44 -0700

    Initial Commit

---
+++ CallCenter.py
@@ -0,0 +1,38 @@
+import time
+
+class Call(object):
+    def __init__(self, callerID, name, number, time, reason):
+        self.id = callerID
+        self.name = name
+        self.number = number
+        self.time = time
+        self.reason = reason
+    def display():
+        print self.id = callerID
+        print self.name = name
+        print self.number = number
+        print self.time = time
+        print self.reason = reason
+
+class CallCenter(object):
+    def __init__(self):
+        self.calls = []
+        self.queue = 0
+    def add(self, callerID, name, number, reason):
+        self.calls.append(Call(callerID, name, number, time.localtime()[3] + (float(time.localtime()[4]) / 100), reason))
+        self.queue += 1
+        return self
+    def remove(self):
+        self.calls.pop(0)
+        self.queue -= 1
+        return self
+    def removeNumber(self, number):
+        for i in range(0, len(self.calls)):
+            if self.calls[i].number == number:
+                self.calls.pop(i)
+                self.queue -= 1
+                break
+        print "Could not locate call"
+        return self
+    def sortCalls(self):
+        self.calls = sorted(self.calls, key=lambda call: call.time)
